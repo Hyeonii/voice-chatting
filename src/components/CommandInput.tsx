7044fed1fadf752f9aa38880fcc1e11d53ae78f8
Nahyeon Kim <nahyeonkim@d-ncomm.ai>
2023-04-03 09:33:51 +0900

    Fix: build 시 나타나는 버그 수정

--- src/components/CommandInput.tsx
+++ src/components/CommandInput.tsx
@@ -21,11 +21,13 @@ function CommandInput({commendText, handleCommandState}: OptionTypes) {
         },
     })
 
-    useEffect(() => {
-        if (transcript.text && transcript.text !== '') {
+    const hadleStopRecording = async () => {
+        await stopRecording();
+
+        if (transcript.text) {
             handleCommandState('commendText', transcript.text);
         }
-    }, [transcript.text]);
+    }
 
     return (
         <div>
@@ -37,7 +39,7 @@ function CommandInput({commendText, handleCommandState}: OptionTypes) {
                 onChange={(e) => handleCommandState('commendText', e.target.value)}
             />
             <button onClick={startRecording}>Start</button>
-            <button onClick={stopRecording}>Stop</button>
+            <button onClick={hadleStopRecording}>Stop</button>
         </div>
     )
 }
