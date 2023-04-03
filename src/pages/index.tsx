6c6f0e31fd54f041aacae6ecb034094cc1299e2a
Nahyeon Kim <nahyeonkim@d-ncomm.ai>
2023-04-03 09:29:53 +0900

    Feat: ver02

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
     return (
         <div>
-            <h1>Home</h1>
+            <h1>Home Ver.02</h1>
             <h3>Whisper test page</h3>
             <Link href="/whisper">Let's start</Link>
         </div>
